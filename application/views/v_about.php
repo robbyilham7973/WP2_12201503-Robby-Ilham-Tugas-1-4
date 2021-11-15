8bc4c419ac263021322911271330592c0f98583b
robbyilham7973 <robbyilham7973@gmail.com>
2021-11-15 15:25:00 +0700

    application Tugas Pertemuan 1-4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Robby</li>
+        <li>Nama Belakang : Ilham</li>
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Cikalang Girang</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tasikmalaya</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Bersepeda</li>
+        <li>Sport</li>
+    </ul>
+</section>
